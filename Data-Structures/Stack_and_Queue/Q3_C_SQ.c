7a5804bf3795a1f4766410de87bdd85ed4f02d26
Y-duchi <du822623@gmail.com>
2026-04-06 15:17:21 +0900

    Q3_C_SQ 해결

--- Data-Structures/Stack_and_Queue/Q3_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q3_C_SQ.c
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section C - Stack and Queue Questions
-Purpose: Implementing the required functions for Question 3 */
+/* CE1007/CZ1007 데이터 구조
+랩 테스트: Section C - 스택과 데주 문제
+목적: 문제 3을 위한 필수 함수 구현 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -10,28 +10,31 @@ Purpose: Implementing the required functions for Question 3 */
 #include <stdlib.h>
 #include <limits.h>
 
-//////////////////////////////////   linked list /////////////////////////////////
+//////////////////////////////////   연결 리스트 /////////////////////////////////
 
-typedef struct _listnode{
-   int item;
-   struct _listnode *next;
+typedef struct _listnode
+{
+	int item;
+	struct _listnode *next;
 } ListNode;
 
-typedef struct _linkedlist{
-   int size;
-   ListNode *head;
-   ListNode *tail;
+typedef struct _linkedlist
+{
+	int size;
+	ListNode *head;
+	ListNode *tail;
 } LinkedList;
 
-////////////////////////////////// stack //////////////////////////////////////////
+////////////////////////////////// 스택 //////////////////////////////////////////
 
-typedef struct stack{
+typedef struct stack
+{
 	LinkedList ll;
 } Stack;
 
-////////////////////////// function prototypes ////////////////////////////////////
+////////////////////////// 함수 프로토타입 ////////////////////////////////////
 
-// You should not change the prototypes of these functions
+// 이 함수들의 프로토타입을 변경하면 안 됩니다
 int isStackPairwiseConsecutive(Stack *s);
 
 void push(Stack *s, int item);
@@ -40,7 +43,7 @@ int peek(Stack *s);
 int isEmptyStack(Stack *s);
 
 void printList(LinkedList *ll);
-ListNode * findNode(LinkedList *ll, int index);
+ListNode *findNode(LinkedList *ll, int index);
 int insertNode(LinkedList *ll, int index, int value);
 int removeNode(LinkedList *ll, int index);
 void removeAllItems(LinkedList *ll);
@@ -49,21 +52,21 @@ void removeAllItems(LinkedList *ll);
 
 int main()
 {
-    int c, value;
+	int c, value;
 
-    Stack s;
+	Stack s;
 
-    s.ll.head=NULL;
-	s.ll.size =0;
-	s.ll.tail =NULL;
+	s.ll.head = NULL;
+	s.ll.size = 0;
+	s.ll.tail = NULL;
 
-    c =1;
+	c = 1;
 
-    printf("1: Insert an integer into the stack:\n");
-    printf("2: Check the stack is pairwise consecutive:\n");
-    printf("0: Quit:\n");
+	printf("1: Insert an integer into the stack:\n");
+	printf("2: Check the stack is pairwise consecutive:\n");
+	printf("0: Quit:\n");
 
-    while (c != 0)
+	while (c != 0)
 	{
 		printf("Please input your choice(1/2/0): ");
 		scanf("%d", &c);
@@ -75,18 +78,19 @@ int main()
 			scanf("%d", &value);
 			push(&s, value);
 			printf("The stack is: ");
-            printList(&(s.ll));
+			printList(&(s.ll));
 			break;
 		case 2:
-            if(isStackPairwiseConsecutive(&s))
-            {
-                printf("The stack is pairwise consecutive.\n");
-            }
-            else{
-                printf("The stack is not pairwise consecutive.\n");
-            }
-            removeAllItems(&(s.ll));
-            break;
+			if (isStackPairwiseConsecutive(&s))
+			{
+				printf("The stack is pairwise consecutive.\n");
+			}
+			else
+			{
+				printf("The stack is not pairwise consecutive.\n");
+			}
+			removeAllItems(&(s.ll));
+			break;
 		case 0:
 			removeAllItems(&(s.ll));
 			break;
@@ -96,45 +100,67 @@ int main()
 		}
 	}
 
-    return 0;
+	return 0;
 }
 
 /////////////////////////////////////////////////////////////////////////////////
 
 int isStackPairwiseConsecutive(Stack *s)
 {
-  /* add your code here */
+	/* 짝이라면 return 1, 짝이 아니라면 return 0 */
+	int size = s->ll.size;
+	
+	if (size % 2 != 0) {
+		return 0;
+	}
+	
+	while (s->ll.head != NULL) {
+		int x = pop(s);
+		int y = pop(s);
+		if(x == y+1){
+			continue;
+		} else {
+			return 0;
+		}
+	}
+	return 1;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 
-void push(Stack *s, int item){
-   insertNode(&(s->ll), 0, item);
+void push(Stack *s, int item)
+{
+	insertNode(&(s->ll), 0, item);
 }
 
-int pop(Stack *s){
-   int item;
-   if(!isEmptyStack(s)){
-    item = ((s->ll).head)->item;
-    removeNode(&(s->ll), 0);
-    return item;
-   }
-    return INT_MIN;
+int pop(Stack *s)
+{
+	int item;
+	if (!isEmptyStack(s))
+	{
+		item = ((s->ll).head)->item;
+		removeNode(&(s->ll), 0);
+		return item;
+	}
+	return INT_MIN;
 }
 
-int peek(Stack *s){
-   return ((s->ll).head)->item;
+int peek(Stack *s)
+{
+	return ((s->ll).head)->item;
 }
 
-int isEmptyStack(Stack *s){
-   if ((s->ll).size == 0)
-      return 1;
-   return 0;
+int isEmptyStack(Stack *s)
+{
+	if ((s->ll).size == 0)
+		return 1;
+	return 0;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 
-void printList(LinkedList *ll){
+void printList(LinkedList *ll)
+{
 
 	ListNode *cur;
 	if (ll == NULL)
@@ -151,7 +177,8 @@ void printList(LinkedList *ll){
 	printf("\n");
 }
 
-ListNode * findNode(LinkedList *ll, int index){
+ListNode *findNode(LinkedList *ll, int index)
+{
 
 	ListNode *temp;
 
@@ -163,7 +190,8 @@ ListNode * findNode(LinkedList *ll, int index){
 	if (temp == NULL || index < 0)
 		return NULL;
 
-	while (index > 0){
+	while (index > 0)
+	{
 		temp = temp->next;
 		if (temp == NULL)
 			return NULL;
@@ -173,15 +201,17 @@ ListNode * findNode(LinkedList *ll, int index){
 	return temp;
 }
 
-int insertNode(LinkedList *ll, int index, int value){
+int insertNode(LinkedList *ll, int index, int value)
+{
 
 	ListNode *pre, *cur;
 
 	if (ll == NULL || index < 0 || index > ll->size + 1)
 		return -1;
 
-	// If empty list or inserting first node, need to update head pointer
-	if (ll->head == NULL || index == 0){
+	// 빈 리스트이거나 첫 번째 노드를 삽입하는 경우, head 포인터를 업데이트해야 합니다
+	if (ll->head == NULL || index == 0)
+	{
 		cur = ll->head;
 		ll->head = malloc(sizeof(ListNode));
 		ll->head->item = value;
@@ -190,10 +220,10 @@ int insertNode(LinkedList *ll, int index, int value){
 		return 0;
 	}
 
-
-	// Find the nodes before and at the target position
-	// Create a new node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	// 목표 위치 이전의 노드를 찾습니다
+	// 새 노드를 생성하고 링크를 다시 연결합니다
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 		cur = pre->next;
 		pre->next = malloc(sizeof(ListNode));
 		pre->next->item = value;
@@ -205,8 +235,8 @@ int insertNode(LinkedList *ll, int index, int value){
 	return -1;
 }
 
-
-int removeNode(LinkedList *ll, int index){
+int removeNode(LinkedList *ll, int index)
+{
 
 	ListNode *pre, *cur;
 
@@ -215,7 +245,8 @@ int removeNode(LinkedList *ll, int index){
 		return -1;
 
 	// If removing first node, need to update head pointer
-	if (index == 0){
+	if (index == 0)
+	{
 		cur = ll->head->next;
 		free(ll->head);
 		ll->head = cur;
@@ -226,7 +257,8 @@ int removeNode(LinkedList *ll, int index){
 
 	// Find the nodes before and after the target position
 	// Free the target node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 
 		if (pre->next == NULL)
 			return -1;
@@ -246,7 +278,8 @@ void removeAllItems(LinkedList *ll)
 	ListNode *cur = ll->head;
 	ListNode *tmp;
 
-	while (cur != NULL){
+	while (cur != NULL)
+	{
 		tmp = cur->next;
 		free(cur);
 		cur = tmp;
