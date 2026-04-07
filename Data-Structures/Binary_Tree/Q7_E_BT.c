3c007f9dcf5adb8b45e13225c6beaf85bf750cc9
Y-duchi <du822623@gmail.com>
2026-04-07 18:46:04 +0900

    Q7_E_BT 해결

--- Data-Structures/Binary_Tree/Q7_E_BT.c
+++ Data-Structures/Binary_Tree/Q7_E_BT.c
@@ -1,13 +1,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section E - Binary Trees Questions
-Purpose: Implementing the required functions for Question 7 */
+/* CE1007/CZ1007 데이터 구조
+랩 테스트: Section E - 이진트리 문제
+목적: 문제 7을 위한 필수 함수 구현 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -16,7 +17,7 @@ typedef struct _btnode
     int item;
     struct _btnode *left;
     struct _btnode *right;
-} BTNode;   // You should not change the definition of BTNode
+} BTNode; // BTNode의 정의를 변경하면 안 됩니다
 
 /////////////////////////////////////////////////////////////////////////////////
 
@@ -31,17 +32,16 @@ typedef struct _stack
     StackNode *top;
 } Stack;
 
+///////////////////////// 함수 프로토타입 ////////////////////////////////////
 
-///////////////////////// Function prototypes ////////////////////////////////////
-
-// You should not change the prototypes of these functions
+// 이 함수들의 프로타입을 변경하면 안 됩니다
 int smallestValue(BTNode *node);
 
 BTNode *createBTNode(int item);
 
 BTNode *createTree();
-void push( Stack *stack, BTNode *node);
-BTNode* pop(Stack *stack);
+void push(Stack *stack, BTNode *node);
+BTNode *pop(Stack *stack);
 
 void printTree(BTNode *node);
 void removeAll(BTNode **node);
@@ -61,13 +61,12 @@ int main()
     printf("2: Smallest value;\n");
     printf("0: Quit;\n");
 
-
-    while(c != 0)
+    while (c != 0)
     {
         printf("Please input your choice(1/2/0): ");
-        if( scanf("%d",&c) > 0)
+        if (scanf("%d", &c) > 0)
         {
-            switch(c)
+            switch (c)
             {
             case 1:
                 removeAll(&root);
@@ -78,7 +77,7 @@ int main()
                 break;
             case 2:
                 value = smallestValue(root);
-                printf("Smallest value of the binary tree is: %d\n",value);
+                printf("Smallest value of the binary tree is: %d\n", value);
                 removeAll(&root);
                 break;
             case 0:
@@ -91,9 +90,8 @@ int main()
         }
         else
         {
-            scanf("%c",&e);
+            scanf("%c", &e);
         }
-
     }
     return 0;
 }
@@ -102,7 +100,28 @@ int main()
 
 int smallestValue(BTNode *node)
 {
-	/* add your code here */
+    /* 주어진 트리에 저장된 가장 작은 값을 반환
+        25 30 65 50 10 60 75 에서 가장 작은 값은 10
+    */
+
+    // 지금 들어온 노드가 없으면, 비교에 방해되지 않는 아주 큰 값을 돌려준다
+    if (node == NULL)
+    {
+        return INT_MAX;
+    }
+
+    int value = node->item;
+
+    int left = smallestValue(node->left);
+    int right = smallestValue(node->right);
+
+    // 현재 값과 왼쪽중에 더 작은 값 리턴
+    int minCurLeft = (value > left) ? left : value;
+    // 위에서 찾은값과 오른쪽 값 중에 더 작은 값 리턴
+    int minRight = (minCurLeft > right) ? right : minCurLeft;
+
+    return minRight;
+
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -118,7 +137,6 @@ BTNode *createBTNode(int item)
 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 BTNode *createTree()
 {
     Stack stack;
@@ -130,57 +148,57 @@ BTNode *createTree()
     root = NULL;
     printf("Input an integer that you want to add to the binary tree. Any Alpha value will be treated as NULL.\n");
     printf("Enter an integer value for the root: ");
-    if(scanf("%d",&item) > 0)
+    if (scanf("%d", &item) > 0)
     {
         root = createBTNode(item);
-        push(&stack,root);
+        push(&stack, root);
     }
     else
     {
-        scanf("%c",&s);
+        scanf("%c", &s);
     }
 
-    while((temp =pop(&stack)) != NULL)
+    while ((temp = pop(&stack)) != NULL)
     {
 
         printf("Enter an integer value for the Left child of %d: ", temp->item);
 
-        if(scanf("%d",&item)> 0)
+        if (scanf("%d", &item) > 0)
         {
             temp->left = createBTNode(item);
         }
         else
         {
-            scanf("%c",&s);
+            scanf("%c", &s);
         }
 
         printf("Enter an integer value for the Right child of %d: ", temp->item);
-        if(scanf("%d",&item)>0)
+        if (scanf("%d", &item) > 0)
         {
             temp->right = createBTNode(item);
         }
         else
         {
-            scanf("%c",&s);
+            scanf("%c", &s);
         }
 
-        if(temp->right != NULL)
-            push(&stack,temp->right);
-        if(temp->left != NULL)
-            push(&stack,temp->left);
+        if (temp->right != NULL)
+            push(&stack, temp->right);
+        if (temp->left != NULL)
+            push(&stack, temp->left);
     }
     return root;
 }
 
-void push( Stack *stack, BTNode *node)
+void push(Stack *stack, BTNode *node)
 {
     StackNode *temp;
 
     temp = malloc(sizeof(StackNode));
-    if(temp == NULL)
+    if (temp == NULL)
         return;
     temp->btnode = node;
-    if(stack->top == NULL)
+    if (stack->top == NULL)
     {
         stack->top = temp;
         temp->next = NULL;
@@ -192,14 +210,14 @@ void push( Stack *stack, BTNode *node)
     }
 }
 
-BTNode* pop(Stack *stack)
+BTNode *pop(Stack *stack)
 {
     StackNode *temp, *top;
     BTNode *ptr;
     ptr = NULL;
 
     top = stack->top;
-    if(top != NULL)
+    if (top != NULL)
     {
         temp = top->next;
         ptr = top->btnode;
@@ -213,16 +231,17 @@ BTNode* pop(Stack *stack)
 
 void printTree(BTNode *node)
 {
-    if(node == NULL) return;
+    if (node == NULL)
+        return;
 
     printTree(node->left);
-    printf("%d ",node->item);
+    printf("%d ", node->item);
     printTree(node->right);
 }
 
 void removeAll(BTNode **node)
 {
-    if(*node != NULL)
+    if (*node != NULL)
     {
         removeAll(&((*node)->left));
         removeAll(&((*node)->right));
