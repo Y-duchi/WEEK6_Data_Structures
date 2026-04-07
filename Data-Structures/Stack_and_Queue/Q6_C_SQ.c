cf068256c0990cee723009e5d056a661327655cd
Y-duchi <du822623@gmail.com>
2026-04-07 15:16:49 +0900

    Q6_C_SQ 해결

--- Data-Structures/Stack_and_Queue/Q6_C_SQ.c
+++ Data-Structures/Stack_and_Queue/Q6_C_SQ.c
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////
 
-/* CE1007/CZ1007 Data Structures
-Lab Test: Section C - Stack and Queue Questions
-Purpose: Implementing the required functions for Question 6 */
+/* CE1007/CZ1007 데이터 구조
+랩 테스트: Section C - 스택과 데주 문제
+목적: 문제 6을 위한 필수 함수 구현 */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -17,22 +17,22 @@ typedef struct _listnode
 {
 	int item;
 	struct _listnode *next;
-} ListNode;	// You should not change the definition of ListNode
+} ListNode; // ListNode의 정의를 변경하면 안 됩니다
 
 typedef struct _linkedlist
 {
 	int size;
 	ListNode *head;
-} LinkedList;	// You should not change the definition of LinkedList
+} LinkedList; // LinkedList의 정의를 변경하면 안 됩니다
 
-
-typedef struct stack{
+typedef struct stack
+{
 	LinkedList ll;
-} Stack; // You should not change the definition of stack
+} Stack; // Stack의 정의를 변경하면 안 됩니다
 
-///////////////////////// function prototypes ////////////////////////////////////
+///////////////////////// 함수 프로토타입 ////////////////////////////////////
 
-// You should not change the prototypes of these functions
+// 이 함수들의 프로토타입을 변경하면 안 됩니다
 void removeUntil(Stack *s, int value);
 
 void push(Stack *s, int item);
@@ -43,7 +43,7 @@ void removeAllItemsFromStack(Stack *s);
 
 void printList(LinkedList *ll);
 void removeAllItems(LinkedList *ll);
-ListNode * findNode(LinkedList *ll, int index);
+ListNode *findNode(LinkedList *ll, int index);
 int insertNode(LinkedList *ll, int index, int value);
 int removeNode(LinkedList *ll, int index);
 
@@ -57,18 +57,17 @@ int main()
 	LinkedList ll;
 	Stack s;
 
-	// Initialize the linked list as an empty linked list
+	// 연결 리스트를 빈 연결 리스트로 초기화합니다
 	ll.head = NULL;
 	ll.size = 0;
 
-	// Initalize the stack as an empty stack
+	// 스택을 빈 스택으로 초기화합니다
 	s.ll.head = NULL;
 	s.ll.size = 0;
 
 	printf("1: Insert an integer into the stack:\n");
-	printf("3: Remove values until the given value;\n");
+	printf("2: Remove values until the given value;\n");
 	printf("0: Quit:\n");
-
 
 	while (c != 0)
 	{
@@ -85,9 +84,9 @@ int main()
 			printList(&(s.ll));
 			break;
 		case 2:
-		    printf("Enter an integer value in stack to remove values until that value: ");
+			printf("Enter an integer value in stack to remove values until that value: ");
 			scanf("%d", &i);
-			removeUntil(&s,i); // You need to code this function
+			removeUntil(&s, i); // 이 함수를 코드해야 합니다
 			printf("The resulting stack after removing values until the given value: ");
 			printList(&(s.ll));
 			removeAllItemsFromStack(&s);
@@ -101,7 +100,6 @@ int main()
 			printf("Choice unknown;\n");
 			break;
 		}
-
 	}
 
 	return 0;
@@ -111,7 +109,19 @@ int main()
 
 void removeUntil(Stack *s, int value)
 {
-/* add your code here */
+	/* 선택된 값이 처음 등장할 때까지 정수 스택에서 모든 값을 제거 
+		1 2 3 4 5 6 7 일 때, 4인 스택을 호출하면 4 5 6 7
+		10 20 15 25 5 일 때, 15인 스택을 호출하면 15 25 5
+	*/	
+
+	// 연결리스트가 0이 될때까지 탐색
+	while (s->ll.size != 0) {
+		int x = pop(s);		// 스택에서 원소를 하나 꺼냄
+		if (x == value) {	// 꺼낸 원소와 찾고자 하는 값이 같다면 현재 원소를 다시 넣고 바로 리턴 
+			push(s, x);
+			return;
+		}
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -126,13 +136,13 @@ void removeAllItemsFromStack(Stack *s)
 	}
 }
 
-
 void removeAllItems(LinkedList *ll)
 {
 	ListNode *cur = ll->head;
 	ListNode *tmp;
 
-	while (cur != NULL){
+	while (cur != NULL)
+	{
 		tmp = cur->next;
 		free(cur);
 		cur = tmp;
@@ -161,11 +171,12 @@ int pop(Stack *s)
 		return MIN_INT;
 }
 
-int peek(Stack *s){
-    if(isEmptyStack(s))
-        return MIN_INT;
-    else
-        return ((s->ll).head)->item;
+int peek(Stack *s)
+{
+	if (isEmptyStack(s))
+		return MIN_INT;
+	else
+		return ((s->ll).head)->item;
 }
 
 int isEmptyStack(Stack *s)
@@ -176,8 +187,8 @@ int isEmptyStack(Stack *s)
 		return 0;
 }
 
-
-void printList(LinkedList *ll){
+void printList(LinkedList *ll)
+{
 
 	ListNode *cur;
 	if (ll == NULL)
@@ -194,7 +205,8 @@ void printList(LinkedList *ll){
 	printf("\n");
 }
 
-ListNode * findNode(LinkedList *ll, int index){
+ListNode *findNode(LinkedList *ll, int index)
+{
 
 	ListNode *temp;
 
@@ -206,7 +218,8 @@ ListNode * findNode(LinkedList *ll, int index){
 	if (temp == NULL || index < 0)
 		return NULL;
 
-	while (index > 0){
+	while (index > 0)
+	{
 		temp = temp->next;
 		if (temp == NULL)
 			return NULL;
@@ -216,7 +229,8 @@ ListNode * findNode(LinkedList *ll, int index){
 	return temp;
 }
 
-int insertNode(LinkedList *ll, int index, int value){
+int insertNode(LinkedList *ll, int index, int value)
+{
 
 	ListNode *pre, *cur;
 
@@ -224,7 +238,8 @@ int insertNode(LinkedList *ll, int index, int value){
 		return -1;
 
 	// If empty list or inserting first node, need to update head pointer
-	if (ll->head == NULL || index == 0){
+	if (ll->head == NULL || index == 0)
+	{
 		cur = ll->head;
 		ll->head = malloc(sizeof(ListNode));
 		if (ll->head == NULL)
@@ -237,10 +252,10 @@ int insertNode(LinkedList *ll, int index, int value){
 		return 0;
 	}
 
-
 	// Find the nodes before and at the target position
 	// Create a new node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 		cur = pre->next;
 		pre->next = malloc(sizeof(ListNode));
 		if (pre->next == NULL)
@@ -256,8 +271,8 @@ int insertNode(LinkedList *ll, int index, int value){
 	return -1;
 }
 
-
-int removeNode(LinkedList *ll, int index){
+int removeNode(LinkedList *ll, int index)
+{
 
 	ListNode *pre, *cur;
 
@@ -266,7 +281,8 @@ int removeNode(LinkedList *ll, int index){
 		return -1;
 
 	// If removing first node, need to update head pointer
-	if (index == 0){
+	if (index == 0)
+	{
 		cur = ll->head->next;
 		free(ll->head);
 		ll->head = cur;
@@ -276,7 +292,8 @@ int removeNode(LinkedList *ll, int index){
 
 	// Find the nodes before and after the target position
 	// Free the target node and reconnect the links
-	if ((pre = findNode(ll, index - 1)) != NULL){
+	if ((pre = findNode(ll, index - 1)) != NULL)
+	{
 
 		if (pre->next == NULL)
 			return -1;
